dca735afbd5682dcd58a71f37b7f6fa27afb61f0
Luciano Scabora <lucianoscabora@gmail.com>
2023-01-26 17:32:30 -0300

    components finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Paginate from './components/Paginate';
+import './style/style.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [animes, setAnimes] = useState({});
+    const [currentPage, setCurrentPage] = useState(3);
+    const [postsPerPage] = useState(5);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+    const testData = animes.data;
+    const animesPerPage = testData?.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+     };
+   
+    useEffect(() => {
+        axios.get('https://kitsu.io/api/edge/anime?page[limit]=20')
+        .then(response => {
+            setAnimes(response.data);
+        })
+    }, [])
+
+
+    const previousPage = () => {
+        if(currentPage !== 1) {
+            setCurrentPage(currentPage -1);
+        }
+    }
+
+    const nextPage = () => {
+        if (currentPage !== Math.ceil(animes.data.length / postsPerPage)) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+   
+return (
+  <div className="main-container">
+        {animesPerPage && ( 
+          // fazer um map para converter no JSX e apresentar na tela
+                <>
+                <ul className="animes-list">
+                    {animesPerPage?.map((item) => (
+                        <li key={item.id} className="list-items">
+                            <img src={item.attributes.posterImage.small} alt="each anime images" />
+                            <br />{item.attributes.canonicalTitle}
+                            <br /> Quantidade de episódios: {item.attributes.episodeCount}
+                        </li>
+                    ))}
+
+                </ul>
+                
+                <Paginate
+                    postsPerPage={postsPerPage}
+                    totalPosts={animes.data.length}
+                    paginate={paginate}
+                    previousPage={previousPage}
+                    nextPage={nextPage} /> 
+                    </>
+          
+                    )}
+  </div>
+);
 }
 
 export default App;
